6f2c911a85d2c8cd0065555706dccba45d2d4149
Kudzai Nyandoro <kudzain@gmail.com>
2018-12-08 15:21:24 -0800

    Update.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -13,7 +13,6 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """Something specific learning about a topic"""
-
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
