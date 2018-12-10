237735b968d92bad7b9ebd3a034bd87af377f25c
Kudzai Nyandoro <kudzain@gmail.com>
2018-12-09 16:46:42 -0800

    Add an index page.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,7 @@
+"""Difine the index page."""
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """Learning logs home page."""
+    return render(request, 'learning_logs/index.html')
+
